8d4271f41e7c094655f27465290d8c1b85771b3b
JDK <jdkranzler@yahoo.com>
2021-02-04 11:52:39 -0600

    halfway there

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 //  put nav and name and checkout price here.
 
 import React from 'react';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import { useHistory } from 'react-router-dom';
+import './Header.css'; 
 
 function Header() {
 
@@ -9,6 +11,14 @@ function Header() {
         <div>
             <header className='App-header'>
                 <h1 className='App-title'>Prime Pizza</h1>
+                <nav>
+                    <ul>
+                        <li> <Link to="/">Home</Link></li>
+                        <li><Link to="/checkout">Checkout</Link></li>
+                        <li><Link to="/customerinfo">CustomerInfo</Link></li>
+                    </ul>
+                </nav>
+
             </header>
         </div>
     );
